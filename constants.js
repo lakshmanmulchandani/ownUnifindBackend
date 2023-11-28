da090052276ce89ee76167d685b164dda5d8cadf
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2023-11-28 19:40:29 +0530

    link added

--- constants.js
+++ constants.js
@@ -4,8 +4,10 @@ import { config } from 'dotenv';
 config();
 
 export const db = process.env.db || "mongodb+srv://lostAndFound:cK0AzTcTOQT3SaEQ@cluster0.sffd2px.mongodb.net/?retryWrites=true&w=majority"
-export const backend_url = process.env.frontend_url || "http://localhost:5000"
-export const frontend_url = process.env.backend_url || "http://localhost:3000"
+// export const backend_url = process.env.frontend_url || "http://localhost:5000"
+export const backend_url = "https://unifind-backend.onrender.com"
+// export const frontend_url = process.env.backend_url || "http://localhost:3000"
+export const frontend_url= "https://unifind.netlify.app"
 
 export const GOOGLE_CLIENT_ID = "360421334611-8tbsqbnufgmep726qtsjh247kr8rjjjv.apps.googleusercontent.com"
 export const GOOGLE_CLIENT_SECRET = "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"
