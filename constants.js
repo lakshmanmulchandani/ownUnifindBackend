91a59a99bddf2c72d1b6f321cb9b45924ea4524e
lakshmanmulchandani <mulchandanilakshman@gmail.com>
2024-03-11 14:49:47 +0530

    backend link updated

--- constants.js
+++ constants.js
@@ -3,11 +3,11 @@ import { config } from 'dotenv';
 
 config();
 
-export const db = process.env.db || "mongodb+srv://lostAndFound:cK0AzTcTOQT3SaEQ@cluster0.sffd2px.mongodb.net/?retryWrites=true&w=majority"
+export const db = process.env.db || "mongodb+srv://lostandfound:gttExu2ExN7TrxHb@cluster0.d9sgkan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // export const backend_url = process.env.frontend_url || "http://localhost:5000"
-export const backend_url = "https://unifind-backend.onrender.com"
+export const backend_url = "https://ownunifindbackend.onrender.com/"
 // export const frontend_url = process.env.backend_url || "http://localhost:3000"
-export const frontend_url= "https://unifind.netlify.app"
+export const frontend_url= "https://unifindnitrr.netlify.app"
 
 export const GOOGLE_CLIENT_ID = "360421334611-8tbsqbnufgmep726qtsjh247kr8rjjjv.apps.googleusercontent.com"
 export const GOOGLE_CLIENT_SECRET = "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"
